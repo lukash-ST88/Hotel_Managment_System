7e38527386d6badf51dd4e08e86511d4e27db3b4
lukash-ST88 <lukash.andrei2012@yandex.ru>
2023-04-18 19:23:33 +0300

    cache is fixed

--- HMS/settings.py
+++ HMS/settings.py
@@ -157,7 +157,16 @@ LOGGING = {
         }
     }
 }
-
+CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379/",
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient"
+            },
+            "KEY_PREFIX": "Hotel_ST88"
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
--- hotel/urls.py
+++ hotel/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import BookingListView, RoomView, CancelBookingView, RoomCategoryListView, logout_user
+from django.views.decorators.cache import cache_page
+
 urlpatterns = [
     path('booking/list/', BookingListView.as_view(), name='bookings'),
     path('room/<str:category>', RoomView.as_view(), name='room'),
     path('booking/cancel/<str:pk>', CancelBookingView.as_view(), name='cancel'),
-    path('', RoomCategoryListView.as_view(), name='rooms'),
+    path('', cache_page(60)(RoomCategoryListView.as_view()), name='rooms'),
     path('logout/', logout_user, name='logout'),
 ]
 
